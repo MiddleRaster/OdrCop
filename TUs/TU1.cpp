3c17db8976b7361efaf1e93e37d35e2e5520bdc3
MiddleRaster <MiddleRaster@users.noreply.github.com>
2026-05-06 23:23:05 -0400

    created RegressionTests.cpp to hold regression tests

--- TUs/TU1.cpp
+++ TUs/TU1.cpp
@@ -53,21 +53,3 @@ auto g_1_f_char  = f<char >();
 auto g_1_f_short = f<short>();
 auto g_1_f_long  = f<long >();
 
-
-// especially tricky code:  a locally (to a function template) defined struct, using template parameter pack
-// copying such code here so that it's included in my regression suite
-#include <windows.h>
-#include <iostream>
-#include "WindowFinder.h"
-void TestWindowFinder()
-{
-                       HWND hwnd = WindowFinder::FindDesiredChildWindow(nullptr, WindowFinder::Has::ClassName{L"#32770"});
-    if (hwnd == nullptr)    hwnd = WindowFinder::FindDesiredChildWindow(nullptr, WindowFinder::Has::ClassName{L"#32770"}, WindowFinder::Is::Visible);
-    if (hwnd == nullptr)    hwnd = WindowFinder::FindDesiredChildWindow(nullptr, WindowFinder::Has::Pid{::GetCurrentProcessId()});
-    if (hwnd == nullptr)    hwnd = WindowFinder::FindDesiredChildWindow(nullptr, WindowFinder::Is::Enabled);
-    if (hwnd == nullptr)    hwnd = WindowFinder::FindDesiredChildWindow(nullptr, WindowFinder::Is::UnOwned);
-    if (hwnd == nullptr)    hwnd = WindowFinder::FindDesiredChildWindow(nullptr, WindowFinder::Is::Enabled, WindowFinder::Is::UnOwned);
-    if (hwnd == nullptr)    hwnd = WindowFinder::FindDesiredChildWindow(nullptr, WindowFinder::Has::ClassName{L"#32770"}, WindowFinder::Is::Enabled, WindowFinder::Is::UnOwned);
-
-    std::wcout << L"WindowFinder found 0x" << std::hex << hwnd << std::dec << L'\n';
-}
--- TUs/TU2.cpp
+++ TUs/TU2.cpp
@@ -53,5 +53,3 @@ auto g_2_f_char  = f<char >();
 auto g_2_f_short = f<short>();
 auto g_2_f_long  = f<long >();
 
-#include <string>
-std::wstring ReturningAnStlTypeIsNotExcludedWhenUsingSwitch() { return L"hi"; }
